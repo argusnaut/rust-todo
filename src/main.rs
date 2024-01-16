eced3ac5c608653b837629a2411a3cc7d8bed339
Vitor Argus <argusvi@outlook.com>
2024-01-16 16:43:32 -0300

    Introduction of finish task, fixes

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,9 @@ use std::{fs, fs::File, io::{stdin, Read}, io};
 use std::path::PathBuf;
 use chrono::Local;
 use dirs::home_dir;
-use lazy_static::lazy_static;
 use uuid::Uuid;
 
-lazy_static! {
-    static ref PATH: PathBuf = {
-        let home = home_dir().expect("Failed to get home directory");
-        let path = home.join(".argus");
-        fs::create_dir_all(&path).expect("Failed to create directory");
-        path.join("data.json")
-    };
-}
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Task {
     id: String,
     description: String,
@@ -33,20 +23,14 @@ impl Task {
         }
     }
 
-    fn add(self, mut tasks: Vec<Task>) -> Vec<Task> {
-        tasks.push(self);
-        write_tasks(&tasks);
-        tasks
-    }
-
     fn display(&self, index: usize) {
-        let done = if self.done { 'x' } else { ' ' };
-        println!("{}. [{}] :: {}", index + 1, done, self.description)
+        let done = if self.done { "[x]" } else { "[ ]" };
+        println!("{:>5} | {:^4} | {}", index + 1, done, self.description)
     }
 }
 
-fn load_tasks() -> Vec<Task> {
-    if let Ok(file) = File::open(&PATH.as_mut_os_str()) {
+fn load_tasks(path: &PathBuf) -> Vec<Task> {
+    if let Ok(file) = File::open(path) {
         let mut file = io::BufReader::new(file);
         let mut json_content = String::new();
         if file.read_to_string(&mut json_content).is_ok() {
@@ -56,21 +40,38 @@ fn load_tasks() -> Vec<Task> {
     Vec::new()
 }
 
-fn write_tasks(vec: &mut Vec<Task>) {
-    if let Err(err) = fs::write(&PATH, serde_json::to_string_pretty(vec)) {
-        eprintln!("Failed to write to file: {}", err);
+fn repeat_char(c: char, count: usize) -> String {
+    std::iter::repeat(c).take(count).collect()
+}
+
+fn list_tasks(tasks: &[Task]) {
+    println!("Index | Done | Description");
+    println!("{} | {} | {}", repeat_char('-', 5), repeat_char('-', 6), repeat_char('-', 11));
+    for (index, task) in tasks.iter().enumerate() {
+        task.display(index);
+    }
+    println!();
+}
+
+fn write_tasks(vec: &mut Vec<Task>, path: &PathBuf) {
+    if let Ok(json_string) = serde_json::to_string_pretty(vec) {
+        if let Err(err) = fs::write(path, json_string) {
+            eprintln!("Failed to write to file: {}", err);
+        }
+    } else {
+        eprintln!("Failed to serialize to JSON");
     }
 }
 
-fn finish_task(task: &Task) {
-    println!("Done: {}", task.id);
+fn finish_task(tasks: &mut [Task], index: usize) {
+    let update_task = tasks.get_mut(index);
+    match update_task {
+        Some(task) => task.done = !task.done,
+        None => eprintln!("Task not found")
+    };
 }
 
-fn menu(tasks: &mut Vec<Task>) {
-    fn repeat_char(c: char, count: usize) -> String {
-        std::iter::repeat(c).take(count).collect()
-    }
-
+fn menu(tasks: &mut Vec<Task>, path: &PathBuf) {
     println!("/{}\\", repeat_char('=', 40));
     println!("|{:^38}|", "Welcome to the Rust Todo list project");
     println!("\\{}/", repeat_char('=', 40));
@@ -91,15 +92,15 @@ fn menu(tasks: &mut Vec<Task>) {
             .expect("Failed to read line");
 
         let user_input = user_input.trim();
+        println!();
 
         match user_input {
             "1" => {
-                for (index, task) in tasks.iter().enumerate() {
-                    task.display(index);
-                }
+                list_tasks(tasks);
             }
             "2" => {
                 let mut description = String::new();
+
                 loop {
                     println!("Please add a description: ");
                     stdin()
@@ -111,14 +112,44 @@ fn menu(tasks: &mut Vec<Task>) {
                         continue;
                     }
                     let new_task = Task::new(description.trim());
-                    tasks = new_task.add(tasks.to_vec());
+                    tasks.push(new_task);
+                    write_tasks(tasks, path);
                     break;
                 }
             }
             "3" => {
-                for task in tasks.iter() {
-                    finish_task(task);
+                loop {
+                    let mut index_str: String = String::new();
+                    list_tasks(tasks);
+
+                    println!("Please select a task by the index:");
+
+                    stdin().read_line(&mut index_str).expect("Failed to read line");
+                    if index_str.trim().is_empty() {
+                        println!("Please select a task by index");
+                        continue;
+                    }
+
+                    let index: usize = match index_str.trim().parse() {
+                        Ok(num) => num,
+                        Err(_) => {
+                            println!("Please insert a number");
+                            continue;
+                        }
+                    };
+
+                    let vec_index = index - 1;
+                    if (0..tasks.len()).contains(&vec_index) {
+                        finish_task(tasks, vec_index);
+                    } else {
+                        println!("Please choose an existing task");
+                        continue;
+                    }
+
+
+                    break;
                 }
+                write_tasks(tasks, path);
             }
             "X" | "x" => break,
             _ => println!("Please choose a valid option"),
@@ -127,6 +158,11 @@ fn menu(tasks: &mut Vec<Task>) {
 }
 
 fn main() {
-    let mut tasks = load_tasks();
-    menu(&mut tasks);
+    let mut path = home_dir().expect("Failed to get home directory").join(".argus");
+    fs::create_dir_all(&path).expect("Failed to create directory");
+    path = path.join("data.json");
+
+    let mut tasks = load_tasks(&path);
+
+    menu(&mut tasks, &path);
 }
